c134584de32b027eb1e6f7c73a2d109a8e48b4c0
John <147920988+jKryptos@users.noreply.github.com>
2024-01-18 22:32:21 -0400

    Removed excessive whiteSpace calls.

--- ProgramStart.java
+++ ProgramStart.java
@@ -7,28 +7,24 @@ public class ProgramStart {
         System.out.println("Welcome to Finance Manager v0.1\nWhat would you like to do?");
         System.out.println("1. Load File\n2. Save File\n3. View Finances\n4. Edit Finances\n5. Exit Program");
         var userInput = scanner.nextLine();
+        Main.whiteSpace(2);
         switch (userInput){
             case "1" :
-                Main.whiteSpace(2);
                 DataStorage.loadFromFile();
                 break;
             case "2" :
-                Main.whiteSpace(2);
                 DataStorage.saveToFile();
                 break;
             case "3" :
-                Main.whiteSpace(2);
                 Category.reportCategory();
                 break;
             case "4" :
-                Main.whiteSpace(2);
                 Editor.financeEditor();
                 break;
             case "5" :
                 exitProgram();
             default:
                 System.out.println("Unexpected value: " + userInput);
-                Main.whiteSpace(2);
                 programStart();
                 break;
         }
